16b2085b109d40a235f365fc3aa0666d3d94a1a3
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-05-01 22:33:26 +0200

    1.0.46

--- templates/index.js
+++ templates/index.js
@@ -1,5 +1,14 @@
 const React = require("react");
-const { Body, Footer, Main, H1, P, Section } = require("react-alegrify-ui");
+const {
+  Body,
+  Footer,
+  Main,
+  H1,
+  H2,
+  H3,
+  P,
+  Section,
+} = require("react-alegrify-ui");
 
 const Head = require("../../components/lib/head");
 
@@ -9,12 +18,52 @@ function Index() {
       <Head title="My amazing website" />
       <Body>
         <Main>
-          <H1>My amazing website</H1>
+          <H1>Static site</H1>
+
           <Section>
-            <P>Put your amazing content over here :)</P>
+            <H2>Project structure</H2>
+
+            <H3>components/src</H3>
+            <P>
+              The `components` folder contains isomorphic components. (both
+              server and client side)
+            </P>
+            <P>
+              Import the `lib` folder variant of a component to use it into
+              pages or clientside js.
+            </P>
+
+            <H3>pages/src</H3>
+            <P>
+              All files directly under `pages/src` will be generated into html
+              pages.
+            </P>
+            <P>For example, `index.js` will become `index.html`</P>
+            <P>These components only render on build time</P>
+
+            <H3>client-js/src</H3>
+            <P>This is all JS that will actually be sent to the client.</P>
+            <P>
+              Every file directly under `client-js/src` will become a separate
+              bundle.
+            </P>
+
+            <H3>static</H3>
+            <P>
+              The static folder contains all assets, pictures, fonts,... that
+              will be copied into the production build
+            </P>
           </Section>
         </Main>
-        <Footer textCenter>Bootstrapped with alegrify-static-site</Footer>
+        <Footer textCenter>
+          This project was bootstrapped with
+          <a
+            className="alegrify-a"
+            href="https://www.npmjs.com/package/alegrify-static-site"
+          >
+            alegrify-static-site
+          </a>
+        </Footer>
       </Body>
     </html>
   );
