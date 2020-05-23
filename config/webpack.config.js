e90ae313e7bb8a26fb3b1ec75a0f6336fd8688e4
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-05-23 15:03:13 +0200

    remove regenerator

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -26,7 +26,6 @@ module.exports = {
             presets: ["@babel/preset-env", "@babel/preset-react"],
             plugins: [
               "@babel/plugin-syntax-dynamic-import",
-              "@babel/plugin-transform-regenerator",
               [
                 "@babel/plugin-transform-runtime",
                 {
