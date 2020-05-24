bf7b85bb902e88ffdfce80391c3c17a12b5bc25d
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-05-24 15:03:49 +0200

    Sanitize output

--- common/mkdir.js
+++ common/mkdir.js
@@ -9,7 +9,7 @@ function mkdir(directoryName) {
     execSync(`mkdir ${directoryName} >/dev/null 2>&1`);
   } catch (ex) {
     // eslint-disable-next-line no-console
-    console.info(`ℹ️ Directory ${directoryName} already exists`);
+    console.info(`ℹ️  Directory ${directoryName} already exists`);
   }
 }
 
