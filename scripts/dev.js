3f8123fdd6876ff1bb9ef52c7130709d0722c33f
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-06-07 20:28:19 +0200

    Improve live server

--- scripts/dev.js
+++ scripts/dev.js
@@ -15,6 +15,7 @@ function runLiveServer() {
   const params = {
     port: 8080,
     root: path.join(process.env.PWD, "./dist"),
+    watch: path.join(process.env.PWD, "./dist"),
     open: true,
   };
   liveServer.start(params);
